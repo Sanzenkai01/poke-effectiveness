237fcc8847490e4abc14670ff82e7baca39efea7
Sanzenkai01 <105128229+Sanzenkai01@users.noreply.github.com>
2026-05-06 02:10:59 -0300

    Home Streamers

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'poke-effectiveness-';
-const CACHE_NAME = `${CACHE_PREFIX}v78`;
+const CACHE_NAME = `${CACHE_PREFIX}v79`;
 const APP_SHELL = [
   new URL('./', self.registration.scope).toString(),
   new URL('./index.html', self.registration.scope).toString(),
@@ -24,7 +24,7 @@ const APP_SHELL = [
   new URL('./bosses/planejador.html', self.registration.scope).toString(),
   new URL('./styles.css?v=20260503e', self.registration.scope).toString(),
   new URL('./script.js?v=20260506b', self.registration.scope).toString(),
-  new URL('./home/home.js?v=20260505d', self.registration.scope).toString(),
+  new URL('./home/home.js?v=20260506a', self.registration.scope).toString(),
   new URL('./route-loader.js', self.registration.scope).toString(),
   new URL('./js/main.js', self.registration.scope).toString(),
   new URL('./bosses/bosses.js?v=20260505d', self.registration.scope).toString(),
