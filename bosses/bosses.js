d463276d57246f814cc17fe2951bf4fb8c31651e
Sanzenkai01 <105128229+Sanzenkai01@users.noreply.github.com>
2026-05-14 03:27:37 -0300

    Daily Bosses

--- bosses/bosses.js
+++ bosses/bosses.js
@@ -6,7 +6,7 @@
 // - 'hidden': esconde o aviso
 // Em `bosses`, voce pode usar nome ou id do boss.
 const hoopaPortalTickerConfig = {
-  mode: 'hidden',
+  mode: 'daily',
   bosses: ['Mega Malamar', 'Mega Lucario'],
   prefix: 'Os Hoopa Portais de hoje sao:',
   customMessage: ''
