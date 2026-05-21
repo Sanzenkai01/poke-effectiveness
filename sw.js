1582619ad23f2740ce09ac45985ca82c84934f15
Sanzenkai01 <105128229+Sanzenkai01@users.noreply.github.com>
2026-05-20 21:06:21 -0300

    Another collor fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'poke-effectiveness-';
-const CACHE_NAME = `${CACHE_PREFIX}v172`;
+const CACHE_NAME = `${CACHE_PREFIX}v173`;
 const APP_SHELL = [
   new URL('./', self.registration.scope).toString(),
   new URL('./index.html', self.registration.scope).toString(),
@@ -24,12 +24,12 @@ const APP_SHELL = [
   new URL('./bosses/mewtwo.html', self.registration.scope).toString(),
   new URL('./bosses/planejador.html', self.registration.scope).toString(),
   new URL('./mouse.png', self.registration.scope).toString(),
-  new URL('./styles.css?v=20260520k', self.registration.scope).toString(),
+  new URL('./styles.css?v=20260520l', self.registration.scope).toString(),
   new URL('./script.js?v=20260520b', self.registration.scope).toString(),
   new URL('./home/home.js?v=20260509a', self.registration.scope).toString(),
   new URL('./js/streamers.shared.js?v=20260509a', self.registration.scope).toString(),
   new URL('./js/visits.shared.js?v=20260520i', self.registration.scope).toString(),
-  new URL('./route-loader.js?v=20260520b', self.registration.scope).toString(),
+  new URL('./route-loader.js?v=20260520c', self.registration.scope).toString(),
   new URL('./js/main.js', self.registration.scope).toString(),
   new URL('./bosses/bosses.js?v=20260518a', self.registration.scope).toString(),
   new URL('./times/teams.json?v=20260517a', self.registration.scope).toString(),
