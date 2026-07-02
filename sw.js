c2bfa274b5e32deb9c1fa9490eaccefbf20a27ae
Sanzenkai01 <105128229+Sanzenkai01@users.noreply.github.com>
2026-07-02 19:54:39 -0300

    aliases

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'poke-effectiveness-';
-let CACHE_NAME = `${CACHE_PREFIX}v793`;
+let CACHE_NAME = `${CACHE_PREFIX}v794`;
 const APP_SHELL = [
   new URL('./', self.registration.scope).toString(),
   new URL('./index.html', self.registration.scope).toString(),
@@ -38,9 +38,9 @@ const APP_SHELL = [
   new URL('./mouse.png', self.registration.scope).toString(),
   new URL('./mega-stone.png', self.registration.scope).toString(),
   new URL('./styles.css?v=20260630e', self.registration.scope).toString(),
-  new URL('./script.js?v=20260702g', self.registration.scope).toString(),
+  new URL('./script.js?v=20260702h', self.registration.scope).toString(),
   new URL('./hunt-builder/hunt_symbol.png?v=20260627a', self.registration.scope).toString(),
-  new URL('./pokemons/pokemons.json?v=20260702c', self.registration.scope).toString(),
+  new URL('./pokemons/pokemons.json?v=20260702d', self.registration.scope).toString(),
   new URL('./pokemons/mega-pokemons.json?v=20260630c', self.registration.scope).toString(),
   new URL('./pokemons/generations.json?v=20260630a', self.registration.scope).toString(),
   new URL('./pokemons/pokedex.json?v=20260629a', self.registration.scope).toString(),
