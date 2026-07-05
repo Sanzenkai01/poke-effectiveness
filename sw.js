78a10c40a09f6dccd324f38346de89b241434cf6
Sanzenkai01 <105128229+Sanzenkai01@users.noreply.github.com>
2026-07-05 20:02:48 -0300

    Novo Visual de fundo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'poke-effectiveness-';
-let CACHE_NAME = `${CACHE_PREFIX}v832`;
+let CACHE_NAME = `${CACHE_PREFIX}v833`;
 const APP_SHELL = [
   new URL('./', self.registration.scope).toString(),
   new URL('./index.html', self.registration.scope).toString(),
@@ -42,7 +42,7 @@ const APP_SHELL = [
   new URL('./bosses/planejador.html', self.registration.scope).toString(),
   new URL('./mouse.png', self.registration.scope).toString(),
   new URL('./mega-stone.png', self.registration.scope).toString(),
-  new URL('./styles.css?v=20260704j', self.registration.scope).toString(),
+  new URL('./styles.css?v=20260705b', self.registration.scope).toString(),
   new URL('./script.js?v=20260705a', self.registration.scope).toString(),
   new URL('./hunt-builder/hunt_symbol.png?v=20260627a', self.registration.scope).toString(),
   new URL('./pokemons/pokemons.json?v=20260702d', self.registration.scope).toString(),
